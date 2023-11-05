6f27fa9e7df4f13129d477604c84824245625f22
vasyl.moshun <vasyl.moshun@gmail.com>
2023-11-05 15:49:10 +0200

    checkstyle added

---
+++ src/test/java/org/moshun/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moshun.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
