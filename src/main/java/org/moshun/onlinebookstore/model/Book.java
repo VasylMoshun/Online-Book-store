0fe033b4d24cf3e51c5c748109e6b2a8daf3219b
vasyl.moshun <vasyl.moshun@gmail.com>
2023-11-05 16:33:07 +0200

    book entity added

---
+++ src/main/java/org/moshun/onlinebookstore/model/Book.java
@@ -0,0 +1,33 @@
+package org.moshun.onlinebookstore.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Entity
+@Table(name = "books")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String author;
+    @NotNull
+    @Column(unique = true)
+    private String isbn;
+    @NotNull
+    @Positive
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
---
+++ src/main/java/org/moshun/onlinebookstore/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package org.moshun.onlinebookstore.repository;
+
+import java.util.List;
+import org.moshun.onlinebookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book save(Book book);
+    List<Book> findAll();
+}
