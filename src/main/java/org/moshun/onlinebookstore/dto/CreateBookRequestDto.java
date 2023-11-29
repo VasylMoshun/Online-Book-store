d9171c1a17d8585c2024ba376e558464d9bacca6
vasyl.moshun <vasyl.moshun@gmail.com>
2023-11-29 17:23:25 +0200

    added GEH

--- src/main/java/org/moshun/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/org/moshun/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,7 +1,10 @@
 package org.moshun.onlinebookstore.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -9,17 +12,21 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class CreateBookRequestDto {
-    @NotBlank
+    @NotBlank(message = "Title cannot be null.")
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "Title cannot be null.")
     private String author;
 
     @NotBlank
+    @Size (min = 1, max = 13, message = "ISBN must be between 1 and 13 characters.")
     private String isbn;
 
-    @Positive
+    @Positive(message = "Price must be more than 0")
+    @NotNull(message = "Price cannot be null")
     private BigDecimal price;
+
+    @Max(100)
     private String description;
     private String coverImage;
 }
